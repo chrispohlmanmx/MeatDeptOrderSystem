e1cefbe034ed126178900fae5717161e0b5d652f
Chris Pohlman <c.pohlmanmx+github@gmail.com>
2021-12-12 20:12:35 -0600

    put in some additional seed data

--- MeatDeptOrderSystem/Migrations/20211213015811_seedOrders.Designer.cs
+++ MeatDeptOrderSystem/Migrations/20211213015811_seedOrders.Designer.cs
@@ -4,14 +4,16 @@ using MeatDeptOrderSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MeatDeptOrderSystem.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20211213015811_seedOrders")]
+    partial class seedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MeatDeptOrderSystem/Models/OrderContext.cs
+++ MeatDeptOrderSystem/Models/OrderContext.cs
@@ -30,7 +30,65 @@ namespace MeatDeptOrderSystem.Models
                         pickupDate = DateTime.Today,
                         LocatedIn = OrderItem.Locations.none,
                         IsReady = true
+                    },
+                    new OrderItem
+                    {
+                        OrderItemId = 2,
+                        FirstName = "Jane",
+                        LastName = "Doe",
+                        Phone = "5553335544",
+                        ItemName = "Ham",
+                        ItemBrand = "hy-vee",
+                        Quantity = 1,
+                        Weight = 12,
+                        pickupDate = DateTime.Parse("08/15/2021 08:30 AM"),
+                        LocatedIn = OrderItem.Locations.none,
+                        IsReady = false
+                    },
+                    new OrderItem
+                    {
+                        OrderItemId = 6,
+                        FirstName = "Mark",
+                        LastName = "Doe",
+                        Phone = "5553335566",
+                        ItemName = "Ham steak",
+                        ItemBrand = "hy-vee",
+                        Quantity = 1,
+                        pickupDate = DateTime.Parse("12/15/2021 08:30 AM"),
+                        LocatedIn = OrderItem.Locations.none,
+                        IsReady = false
+                    },
+                    new OrderItem
+                    {
+                        OrderItemId = 5,
+                        FirstName = "Mark",
+                        LastName = "Brandanawitz",
+                        Phone = "5553335566",
+                        ItemName = "Pigs feet",
+                        Quantity = 1,
+                        pickupDate = DateTime.Parse("12/15/2021 08:30 AM"),
+                        LocatedIn = OrderItem.Locations.none,
+                        IsReady = false,
+                        CuttingInstructions= "cut into 3 pieces",
+                        PackagingInstructions="6 pieces to a package",
+                        OtherComments="They want 1 whole case"
+                    },
+                    new OrderItem
+                    {
+                        OrderItemId = 4,
+                        FirstName = "Jack",
+                        LastName = "Doe",
+                        Phone = "5552235566",
+                        ItemName = "Boneless Rib Roast",
+                        Quantity = 1,
+                        Weight = 6,
+                        pickupDate = DateTime.Parse("12/24/2021 08:30 AM"),
+                        LocatedIn = OrderItem.Locations.none,
+                        IsReady = false,
+                        CuttingInstructions = "trim a little extra fat off"
                     }
+
+
                 );
         }
           
